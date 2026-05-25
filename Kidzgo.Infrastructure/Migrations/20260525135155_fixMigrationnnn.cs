5a4f6610ce8791ecad88fe54fa5292907cd110bc
Nguyn <phamphuckhang24@gmail.com>
2026-05-25 20:53:00 +0700

    fix migration

---
+++ Kidzgo.Infrastructure/Migrations/20260525135155_fixMigrationnnn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kidzgo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixMigrationnnn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_LessonPlanTemplates_ModuleId",
+                schema: "public",
+                table: "LessonPlanTemplates",
+                column: "ModuleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LessonPlanTemplates_ModuleId",
+                schema: "public",
+                table: "LessonPlanTemplates");
+        }
+    }
+}
